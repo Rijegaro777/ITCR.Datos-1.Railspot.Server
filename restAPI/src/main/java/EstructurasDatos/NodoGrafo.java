45b327ee545a8413bf7ae0b755788f12835156a1
Fabio Villalobos <fofov31@gmail.com>
2020-08-08 18:37:30 -0600

    Agregadas clases faltantes para el grafo

--- restAPI/src/main/java/EstructurasDatos/NodoGrafo.java
+++ restAPI/src/main/java/EstructurasDatos/NodoGrafo.java
@@ -1,5 +1,27 @@
 package EstructurasDatos;
 
-public class NodoGrafo {
+import java.util.ArrayList;
 
-}
+public class NodoGrafo {
+	  private int distanceFromSource = Integer.MAX_VALUE;
+	  private boolean visited;
+	  private ArrayList<AristaGrafo> edges = new ArrayList<AristaGrafo>(); // now we must create edges
+	  public int getDistanceFromSource() {
+	    return distanceFromSource;
+	  }
+	  public void setDistanceFromSource(int distanceFromSource) {
+	    this.distanceFromSource = distanceFromSource;
+	  }
+	  public boolean isVisited() {
+	    return visited;
+	  }
+	  public void setVisited(boolean visited) {
+	    this.visited = visited;
+	  }
+	  public ArrayList<AristaGrafo> getEdges() {
+	    return edges;
+	  }
+	  public void setEdges(ArrayList<AristaGrafo> edges) {
+	    this.edges = edges;
+	  }
+	}
